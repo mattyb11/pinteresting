c287fa8bb2253d83730e8bdd62b89e3049c5d4e9
Matt Blank <mab5vb@virginia.edu>
2015-01-20 19:08:54 -0500

    Added SQL

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -2,15 +2,82 @@ class PlayersController < ApplicationController
   before_action :set_player, only: [:show, :edit, :update, :destroy]
 
   def index
-    @players = Player.all.order("positional_ranking")
+    @qb = Player.all.find_by_sql(
+      "SELECT player
+      FROM players
+      WHERE position = 'QB'
+      AND positional_ranking = 1")
+
+    @rb1 = Player.find_by_sql(
+      "SELECT player
+      FROM players
+      WHERE position = 'RB'
+      AND positional_ranking = 1")
+
+    @rb2 = Player.find_by_sql(
+      "SELECT player
+      FROM players
+      WHERE position = 'RB'
+      AND positional_ranking = 2")
+  
+    @wr1 = Player.find_by_sql(
+      "SELECT player
+      FROM players
+      WHERE position = 'WR'
+      AND positional_ranking = 1")
+
+    @wr2 = Player.find_by_sql(
+      "SELECT player
+      FROM players
+      WHERE position = 'WR'
+      AND positional_ranking = 2")
+
+    @te = Player.find_by_sql(
+      "SELECT player
+      FROM players
+      WHERE position = 'TE'
+      AND positional_ranking = 1")
+
+    @flex = Player.find_by_sql(
+      "SELECT player
+      FROM players
+      WHERE (position = 'RB' AND positional_ranking != 1 AND positional_ranking != 2)
+      OR (position = 'WR' AND positional_ranking != 1 AND positional_ranking != 2)
+      OR (position = 'TE' AND positional_ranking != 1)
+      GROUP BY overall_ranking")
+
+    @dl = Player.find_by_sql(
+      "SELECT player
+      FROM players
+      WHERE position = 'DL'
+      AND positional_ranking = 1")
+
+    @lb = Player.find_by_sql(
+      "SELECT player
+      FROM players
+      WHERE position = 'LB'
+      AND positional_ranking = 1")
+
+    @db = Player.find_by_sql(
+      "SELECT player
+      FROM players
+      WHERE position = 'DB'
+      AND positional_ranking = 1")
+
+    @k = Player.find_by_sql(
+      "SELECT player
+      FROM players
+      WHERE position = 'K'
+      AND positional_ranking = 1")
   end
+
 
   def rankings
     @players = Player.all.order("overall_ranking")
   end
 
   def depthchart
-    @players = Player.all.order("position")
+    @players = Player.all.order("overall_ranking")
   end
 
   def show
@@ -28,7 +95,7 @@ class PlayersController < ApplicationController
 
     respond_to do |format|
       if @player.save
-        format.html { redirect_to @player, notice: 'Player was successfully created.' }
+        format.html { redirect_to @players, notice: 'Player was successfully created.' }
         format.json { render :show, status: :created, location: @player }
       else
         format.html { render :new }
@@ -40,7 +107,7 @@ class PlayersController < ApplicationController
   def update
     respond_to do |format|
       if @player.update(player_params)
-        format.html { redirect_to @player, notice: 'Player was successfully updated.' }
+        format.html { redirect_to @players, notice: 'Player was successfully updated.' }
         format.json { render :show, status: :ok, location: @player }
       else
         format.html { render :edit }
@@ -50,9 +117,9 @@ class PlayersController < ApplicationController
   end
 
   def destroy
-    @player.destroy
+    @player.delete
     respond_to do |format|
-      format.html { redirect_to players_url, notice: 'Player was successfully destroyed.' }
+      format.html { redirect_to @players, notice: 'Player was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
@@ -65,4 +132,4 @@ class PlayersController < ApplicationController
     def player_params
       params.require(:player).permit(:player, :position, :overall_ranking, :positional_ranking)
     end
-end
+  end
